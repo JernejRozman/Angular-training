ce3a7f6f019e933abd1ceec0e4df75b110c14d5c
Jernej Rozman <93260393+JernejRozman@users.noreply.github.com>
2026-05-08 13:50:11 +0200

    ASDFASDF

--- TypeScript/7-asConstReadonlyImmutables.js
+++ TypeScript/7-asConstReadonlyImmutables.js
@@ -1,3 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const obj = {
+    foo: {
+        bar: 14,
+    },
+};
+const pica = {
+    topping: "salami",
+};
+// pica.topping = "cheese"
+// Protects the atributes of types
+// The immutability of of objects, types and variables is very important when we don't want data to change and we do not allow for change to happen.
 //# sourceMappingURL=7-asConstReadonlyImmutables.js.map
---
+++ TypeScript/AOC20251.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=AOC20251.d.ts.map
