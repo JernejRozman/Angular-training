824e40b7158ddacd3823344a66eab0df8f03beaa
Jernej Rozman <93260393+JernejRozman@users.noreply.github.com>
2026-05-13 09:28:15 +0200

    angular training 12. 05. 2025

--- AngularTrainingProject/src/app/app.ts
+++ AngularTrainingProject/src/app/app.ts
@@ -3,10 +3,19 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('AngularTrainingProject');
+  
+  // 1. Property initialized to 0
+  count = 0;
+
+  // 2. Method to increase count by 1
+  increment() {
+    this.count++;
+  }
 }
