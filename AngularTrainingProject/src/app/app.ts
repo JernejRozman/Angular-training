c9bce1bc47cd1abd5591dba59691c053e54d79b4
Jernej Rozman <93260393+JernejRozman@users.noreply.github.com>
2026-05-15 14:42:04 +0200

    15052026 push

--- AngularTrainingProject/src/app/app.ts
+++ AngularTrainingProject/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -12,10 +12,14 @@ export class App {
   protected readonly title = signal('AngularTrainingProject');
   
   // 1. Property initialized to 0
-  count = 0;
-
+  count : number = 0;
   // 2. Method to increase count by 1
   increment() {
     this.count++;
   }
+  name : string = ""
+
+
+  stevilke : number[]= [1, 2, 3, 4, 5];
+  
 }
